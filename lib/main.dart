a7458d6c2c1545ad0600f04c0c76bcdb430e020a
Nelson Alexandrino <munguambe@outlook.com>
2020-09-11 21:54:43 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_core/core.dart';
+
+import './pages/homepage.dart';
+
+void main() {
+  SyncfusionLicense.registerLicense('NT8mJyc2IWhia31hfWN9Z2doYmF8YGJ8ampqanNiYmlmamlmanMDHmg+Jj00JjI+MTYTPCYnPzw8OH0wPD4='); 
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yami',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
